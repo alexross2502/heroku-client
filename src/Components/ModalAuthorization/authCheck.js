dec9ae0023c69c586deaa1f3f2317db3f1f35284
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2023-01-16 08:33:16 +0200

    fix: logoutButton 2

--- src/Components/ModalAuthorization/authCheck.js
+++ src/Components/ModalAuthorization/authCheck.js
@@ -2,8 +2,7 @@ export async function authCheck(formData) {
   let data = {};
   data.password = formData.password;
   data.login = formData.email;
- // const response = await fetch("https://mysqltest.herokuapp.com/api/admin", {
-  const response = await fetch("http://localhost:3306/api/admin", {
+  const response = await fetch("https://mysqltest.herokuapp.com/api/admin", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
