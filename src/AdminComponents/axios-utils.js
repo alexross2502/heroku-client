21dba976d9dd6586487aebc5bc150e09048bb97f
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2023-01-22 18:35:21 +0200

    test 58

--- src/AdminComponents/axios-utils.js
+++ src/AdminComponents/axios-utils.js
@@ -3,14 +3,15 @@ import { getToken } from "./token";
 
 const client = axios.create({baseURL : 'https://mysqltest.herokuapp.com/api'})
 
-
+//http://localhost:3306/api
+//https://mysqltest.herokuapp.com/api
 export const request = ({...option}) => {
     
     client.defaults.headers.common.Authorization = getToken()
     const onSuccess = responce => responce.data
     const onError = error => {
         if(error.response.status == 401) {
-            const response = axios.get('https://mysqltest.herokuapp.com/api/towns')
+            const response = axios.get('https://mysqltest.herokuapp.com/api')
             if(response.status === 200){
                 console.log('401')
                 return (error.config)
